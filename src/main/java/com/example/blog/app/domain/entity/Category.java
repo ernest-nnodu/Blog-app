36a3b4e8229d05ae0765424752b3e57a8dbd00d1
ernest nnodu <ernest.nnodu@outlook.com>
2025-01-29 18:51:33 +0000

    Create category entity to represent category of a post

---
+++ src/main/java/com/example/blog/app/domain/entity/Category.java
@@ -0,0 +1,23 @@
+package com.example.blog.app.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "categories")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
